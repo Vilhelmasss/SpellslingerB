0ce4f36c4443f5e2765ec4c537e9c402ab7f1ed8
Vilhelmasss <vilhelmasss@gmail.com>
2022-05-23 18:03:18 +0300

    Scene

---
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    private float hitPoints;
+    private float mana;
+    private float manaRegen;
+    private float movSpeed;
+    private float manaThreshold;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
