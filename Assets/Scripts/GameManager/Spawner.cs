6acdf0978a81a7cf1df697ca3448e29261b39371
Vilhelmasss <vilhelmasss@gmail.com>
2022-06-26 09:16:00 +0300

    First spawn working

--- Assets/Scripts/GameManager/Spawner.cs
+++ Assets/Scripts/GameManager/Spawner.cs
@@ -5,9 +5,29 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public List<GameObject> enemyList;
-    public List<GameObject> spawnedEnemy;
+
     public List<Transform> spawner;
     public GameObject spawnVfx;
+
+    public GameObject player;
+
+    void Start()
+    {
+        Wave1();
+    }
+
+    void SpawnSimpleCreature()
+    {
+        Vector3 currentSpawnPosition = spawner[Random.Range(0, 3)].position;
+        Instantiate(spawnVfx, currentSpawnPosition, Quaternion.identity);
+        GameObject enemy = Instantiate(enemyList[Random.Range(0, 1)], currentSpawnPosition, Quaternion.identity);
+        enemy.GetComponent<EnemyNavMeshAI>().movePosTransform = player.transform;
+    }
+
+    void SpawnSummoner()
+    {
+
+    }
 
     void Wave1()
     {
@@ -16,20 +36,55 @@ public class Spawner : MonoBehaviour
             float spawnStart = Random.Range(0f, 3f);
             for (int j = 0; j < Random.Range(0, 2); j++)
             {
-
+                
+                Invoke("SpawnSimpleCreature", spawnStart++);
             }
 
         }
     }
 
-    void SpawnSimpleCreature()
+    void Wave2()
     {
-//        Instantiate()
+        for (int i = 0; i < 3; i++)
+        {
+            float spawnStart = Random.Range(0f, 3f);
+            for (int j = 0; j < Random.Range(0, 10); j++)
+            {
+
+                Invoke("SpawnSimpleCreature", spawnStart++);
+            }
+
+        }
     }
 
-    void SpawnSummoner()
+    void Wave3()
     {
+        for (int i = 0; i < 3; i++)
+        {
+            float spawnStart = Random.Range(0f, 3f);
+            for (int j = 0; j < Random.Range(0, 10); j++)
+            {
 
+                Invoke("SpawnSimpleCreature", spawnStart++);
+            }
+
+        }
     }
+
+    void Wave4()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            float spawnStart = Random.Range(0f, 3f);
+            for (int j = 0; j < Random.Range(0, 10); j++)
+            {
+
+                Invoke("SpawnSimpleCreature", spawnStart++);
+            }
+
+        }
+    }
+
+
 
 }
